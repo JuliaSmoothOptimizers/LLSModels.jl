f476702bcdabf83958c0d463cf867087e66fe138
Geoffroy Leconte <gleconte50@gmail.com>
2021-06-18 11:34:46 -0400

    add tests interaction

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,8 @@
 #stdlib
 using LinearAlgebra, SparseArrays, Test
 #jso
-using NLPModels, LLSModels, NLPModelsTest
+using NLPModels, LLSModels, NLPModelsTest, NLPModelsModifiers
 
 include("test_lls_model.jl")
 include("nls_testutils.jl")
+include("test_interaction.jl")
