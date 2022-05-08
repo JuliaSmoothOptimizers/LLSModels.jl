3930e67e8e84d84d08cfc7f1c37f339b5a40597e
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-05-08 18:14:50 -0400

    Update solve_krylov.jl and test_krylov.jl

--- src/solve_krylov.jl
+++ src/solve_krylov.jl
@@ -63,42 +63,5 @@ list_krylov_solvers = Dict{Symbol, Symbol}()
     """
     @inline Krylov.GpmrSolver(lls::LLSModel, args...; kwargs...) =
       Krylov.GpmrSolver(lls.A, lls.b, args...; kwargs...)
-
-    """
-        cg_lanczos(::LLSModel, shifts, args...; kwargs...)
-
-    The Lanczos version of the conjugate gradient method to solve a family of shifted systems for linear least-squares from Krylov.jl.
-    """
-    function Krylov.cg_lanczos(
-      lls::LLSModel,
-      shifts::AbstractVector{T},
-      args...;
-      kwargs...,
-    ) where {T <: AbstractFloat}
-      unconstrained(lls) || error("The LLSModels has constraints.")
-      Krylov.cg_lanczos(lls.A, lls.b, shifts, args...; kwargs...)
-    end
-    """
-        cg_lanczos!(::CgLanczosShiftSolver, ::LLSModel, args...; kwargs...)
-
-    The in-place Lanczos version of the conjugate gradient method to solve a family of shifted systems for linear least-squares from Krylov.jl.
-    """
-    function Krylov.cg_lanczos!(
-      solver::Krylov.CgLanczosShiftSolver,
-      lls::LLSModel,
-      shifts::AbstractVector{T},
-      args...;
-      kwargs...,
-    ) where {T <: AbstractFloat}
-      unconstrained(lls) || error("The LLSModels has constraints.")
-      Krylov.cg_lanczos!(solver, lls.A, lls.b, shifts, args...; kwargs...)
-    end
-    """
-        CgLanczosShiftSolver(::LLSModel)
-
-    Wrapper to define the solver structure CgLanczosShiftSolver used in the cg_lanczos! in-place method for linear least-squares from Krylov.jl.
-    """
-    @inline Krylov.CgLanczosShiftSolver(lls::LLSModel, args...; kwargs...) =
-      Krylov.CgLanczosShiftSolver(lls.A, lls.b, args...; kwargs...)
   end
 end
