de30d880356759f1e406f12a344ca7ed61e1fa46
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-05-08 18:14:50 -0400

    Update solve_krylov.jl and test_krylov.jl

--- test/test_krylov.jl
+++ test/test_krylov.jl
@@ -7,21 +7,9 @@ using Krylov
   A = spdiagm(-1 => ones(n - 1), 0 => 4 * ones(n), 1 => ones(n - 1))
   b = A * ones(n)
   c = b
-  lls = LLSModel(A, b)
-
-  (x, stats) = cgls(lls)
-  solver = CglsSolver(lls)
-  cgls!(solver, lls)
-  @test stats.solved
-
   shifts = collect(1.0:10.0)
   nshifts = length(shifts)
   lls = LLSModel(A, b)
-  (x, stats) = cg_lanczos(lls, shifts)
-  @test stats.solved
-  solver = CgLanczosShiftSolver(lls, nshifts)
-  cg_lanczos!(solver, lls, shifts)
-  @test stats.solved
 
   for (ofun, KS) in LLSModels.list_krylov_solvers
     if ofun in [:craig, :craigmr, :lnlq]
@@ -36,14 +24,25 @@ using Krylov
     elseif ofun in [:usymlq, :usymqr]
       (x, stats) = eval(ofun)(lls, c)
       @test stats.solved
+    elseif ofun == :cg_lanczos_shift
+      (x, stats) = eval(ofun)(lls, shifts)
+      @test stats.solved
     else
       (x, stats) = eval(ofun)(lls)
       @test stats.solved
     end
-    solver = eval(KS)(lls)
+
+    if KS == :CgLanczosShiftSolver
+      solver = eval(KS)(lls, nshifts)
+    else
+      solver = eval(KS)(lls)
+    end
+
     ifun = Symbol(ofun, "!")
-    if ofun in [:bilqr, :gpmr, :tricg, :trilqr, :trimr, :usymlq, :usymqr]
+    if ifun in [:bilqr!, :gpmr!, :tricg!, :trilqr!, :trimr!, :usymlq!, :usymqr!]
       eval(ifun)(solver, lls, c)
+    elseif ifun == :cg_lanczos_shift!
+      eval(ifun)(solver, lls, shifts)
     else
       eval(ifun)(solver, lls)
     end
